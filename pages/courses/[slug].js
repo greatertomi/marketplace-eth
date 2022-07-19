c5539e29b2a7534b2c173252e77e2d60cbf53d93
John Oshalusi <john.oshalusi@iohk.io>
2022-07-19 22:14:42 +0100

    Work on course slug and improved components

--- pages/courses/[slug].js
+++ pages/courses/[slug].js
@@ -1,16 +1,45 @@
 import { CourseHero, Curriculum, Keypoints } from "@components/course";
 import { Modal } from "@components/common";
 import { BaseLayout } from "@components/layout";
+import { getAllCourses } from "@content/courses/fetcher";
 
-export default function Course() {
+export default function Course({ course }) {
   return (
     <BaseLayout>
       <div className="py-4">
-        <CourseHero />
+        <CourseHero
+          title={course.title}
+          description={course.description}
+          image={course.coverImage}
+        />
       </div>
-      <Keypoints />
-      <Curriculum />
+      <Keypoints points={course.wsl} />
+      <Curriculum locked={true} />
       <Modal />
     </BaseLayout>
   );
+}
+
+export function getStaticPaths() {
+  const { data } = getAllCourses();
+
+  return {
+    paths: data.map((course) => ({
+      params: {
+        slug: course.slug,
+      },
+    })),
+    fallback: false,
+  };
+}
+
+export function getStaticProps({ params }) {
+  const { data } = getAllCourses();
+  const course = data.filter((course) => course.slug === params.slug)[0];
+
+  return {
+    props: {
+      course,
+    },
+  };
 }
