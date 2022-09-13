dae46303d92da97940f6c652d0fd88c4da740570
John Oshalusi <john.oshalusi@iohk.io>
2022-09-14 00:20:10 +0100

    Improve connect button

--- components/ui/common/navbar/index.js
+++ components/ui/common/navbar/index.js
@@ -1,13 +1,17 @@
 import Link from "next/link";
 import { useWeb3 } from "@components/providers";
+import { Button } from "@components/ui/common";
+import { useRouter } from "next/router";
 
 export default function Navbar() {
-  const { connect } = useWeb3();
+  const { connect, isWeb3Loaded, isLoading } = useWeb3();
+  const router = useRouter();
+
   return (
     <section>
       <div className="relative pt-6 px-4 sm:px-6 lg:px-8">
         <nav className="relative" aria-label="Global">
-          <div className="flex justify-between">
+          <div className="flex justify-between items-center">
             <div>
               <Link href="/">
                 <a className="font-medium mr-8 text-gray-500 hover:text-gray-900">
@@ -31,12 +35,22 @@ export default function Navbar() {
                   Wishlist
                 </a>
               </Link>
-              <span
-                onClick={connect}
-                className="px-8 py-3 rounded-md border text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700 cursor-pointer"
-              >
-                Connect
-              </span>
+              {isLoading ? (
+                <Button onClick={connect} disabled={true}>
+                  Loading...
+                </Button>
+              ) : isWeb3Loaded ? (
+                <Button onClick={connect}>Connect</Button>
+              ) : (
+                <Button
+                  onClick={window.open(
+                    "https://metamask.io/download/",
+                    "_blank"
+                  )}
+                >
+                  Install Metamask
+                </Button>
+              )}
             </div>
           </div>
         </nav>
--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import { Hero } from "@components/ui/common";
 import { CourseList } from "@components/ui/course";
 import { BaseLayout } from "@components/ui/layout";
 import { getAllCourses } from "@content/courses/fetcher";
-import { useWeb3 } from "@components/providers";
 
 export default function Home({ courses }) {
   return (
